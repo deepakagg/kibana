321525e31c27d2c24ab88c305f0b67a31771baaf
deepakaggarwal <deepak.aggarwal9@gmail.com>
2019-11-25 15:34:27 +0530

    Added optimise files

---
+++ optimize/bundles/security-configuration.entry.js
@@ -0,0 +1,81 @@
+
+/**
+ * Kibana entry file
+ *
+ * This is programmatically created and updated, do not modify
+ *
+ * context: {
+  "env": "production",
+  "kbnVersion": "7.2.0",
+  "buildNum": 9007199254740991,
+  "plugins": [
+    "apm_oss",
+    "console",
+    "data",
+    "elasticsearch",
+    "input_control_vis",
+    "inspector_views",
+    "interpreter",
+    "kbn_doc_views",
+    "kbn_vislib_vis_types",
+    "kibana",
+    "markdown_vis",
+    "metric_vis",
+    "metrics",
+    "opendistro_security",
+    "opendistro-alerting",
+    "region_map",
+    "state_session_storage_redirect",
+    "status_page",
+    "table_vis",
+    "tagcloud",
+    "testbed",
+    "tests_bundle",
+    "tile_map",
+    "timelion",
+    "ui_metric",
+    "vega"
+  ]
+}
+ */
+
+// ensure the csp nonce is set in the dll
+import 'dll/set_csp_nonce';
+
+// set the csp nonce in the primary webpack bundle too
+__webpack_nonce__ = window.__kbnNonce__;
+
+// import global polyfills
+import '@babel/polyfill';
+import 'custom-event-polyfill';
+import 'whatwg-fetch';
+import 'abortcontroller-polyfill';
+import 'childnode-remove-polyfill';
+
+import { i18n } from '@kbn/i18n';
+import { CoreSystem } from '__kibanaCore__'
+
+const injectedMetadata = JSON.parse(document.querySelector('kbn-injected-metadata').getAttribute('data'));
+
+i18n.load(injectedMetadata.i18n.translationsUrl)
+  .catch(e => e)
+  .then((i18nError) => {
+    const coreSystem = new CoreSystem({
+      injectedMetadata,
+      rootDomElement: document.body,
+      browserSupportsCsp: !window.__kbnCspNotEnforced__,
+      requireLegacyFiles: () => {
+        require('plugins/opendistro_security/apps/configuration/configuration');
+      }
+    });
+
+    coreSystem
+      .setup()
+      .then((coreSetup) => {
+        if (i18nError) {
+          coreSetup.fatalErrors.add(i18nError);
+        }
+
+        return coreSystem.start();
+      });
+  });
